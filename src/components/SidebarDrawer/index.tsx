f8a7990606ccc53157582a458e7ebcbbf92d1426
Aleksandr Ivanov <1300087@gmail.com>
2022-11-25 18:50:23 +0300

    +++

--- src/components/SidebarDrawer/index.tsx
+++ src/components/SidebarDrawer/index.tsx
@@ -6,37 +6,37 @@ import NewSidebarTodoListNameInput from '../NewSidebarTodoListNameInput';
 import SidebarTodoListNameItems from '../SidebarTodoListNameItems';
 import styles from './SidebarDrawer.module.css';
 
-type SidebarDrawerType = {
+type SidebarDrawerPropsType = {
    dataUser: DataUserType | null,
    drawerOpen: boolean,
    drawerClose: () => void
-}
+};
 
-const SidebarDrawer: React.FC<SidebarDrawerType> = (props) => {
+const SidebarDrawer: React.FC<SidebarDrawerPropsType> = ({ drawerOpen, drawerClose, dataUser }) => {
    const [isShowInput, setShowInput] = useState(false);
    const closeInput = () => {
-      setShowInput(false)
-   }
-   const {
-      drawerOpen,
-      drawerClose,
-      dataUser
-   } = props;
+      setShowInput(false);
+   };
+
    return (
       <Drawer
          anchor='left'
          open={drawerOpen}
-         onClose={drawerClose}>
+         onClose={drawerClose}
+      >
          <List sx={{ width: '320px' }}>
             <div className={styles.back}>
                <div className={styles.root}>TODO LISTS</div>
-               <IconButton onClick={() => { setShowInput(true) }}>
+               <IconButton onClick={() => { setShowInput(true); }}>
                   <Add />
                </IconButton>
             </div>
             <Divider />
-            {isShowInput && <NewSidebarTodoListNameInput /*drawerClose={drawerClose}*/ closeInput={closeInput} />}
-            <SidebarTodoListNameItems todoListNames={dataUser?.todoListNames} drawerClose={drawerClose} />
+            {isShowInput && <NewSidebarTodoListNameInput closeInput={closeInput} />}
+            <SidebarTodoListNameItems
+               todoListNames={dataUser?.todoListNames}
+               drawerClose={drawerClose}
+            />
          </List>
       </Drawer>
    );
